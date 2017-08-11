21a98ec89144c77341b68269d9518b81052bb714
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-11 15:49:36 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -20,8 +20,8 @@ for($id = 0; $id <= 0; $id++)
 	{
 		 $info['urlbutton'] = $href->value;
 				for($url = 0; $url <= sizeof($info['urlbutton']); $url++){
-         echo $info['urlbutton'][$url];
-      }
+				echo $info['urlbutton']$url;
+	    }
 		 
 	}
       
