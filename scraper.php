d10f1536598e99904ea425aca00cd4760c14337e
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-18 14:40:16 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,32 +3,47 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
 /** looping over list of ids of doctors **/
-for($id = 0; $id <= 0; $id++)
+$site = 'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
+//This is for Pagination 
+for($page = 0; $page < 1; $page++)
+    {
+        $FinalURL = $site . $page;
+        $html = file_get_html($FinalURL);
+    }
+
+if($html ==true)
+{
+//This is for Table Scrapping
+foreach($html->find("//*[@id='w0']/table/tbody/tr") as $element) 
 	{
-	 $url = ("http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=".$id);
-	$link2 = file_get_html($url);
-  foreach($link2->find("//*[@id='w0']/table/tbody/tr")as $element){
+
 		if(is_object($element))
 	{
 	 	 $info['num'] 		= $element->find("td", 0)->plaintext;
 		 $info['courtname']  	= $element->find("td", 1)->plaintext;
 		 $info['caseno']  		= $element->find("td", 2)->plaintext;
 		 $info['status']  		= $element->find("td", 3)->plaintext;
-		echo $href 			= $element->find(".//td/button", 0);
-			if($href){
-			  $link = $href->onclick;
-    				echo $link;
-			}
+		 $href 			= $element->find(".//td/button", 0);
+			
 				 
-	}
-      
-      
-		}}
-	  
-	  
-// scraperwiki::save_sqlite(array('num'), array('num' => $info['num'],'courtname' => $info['courtname'],'caseno' => $info['caseno'],'status' => $info['status'],'urlbutton' => $info['urlbutton'] ));
-		
+	
 
-// called "data".
-// called "data".
+	if(is_object($href))
+	{
+		 $urlbutton = $href->value;
+			
+			 $localvar = 'http://202.61.43.40:8080/' . $urlbutton;
+			  
+			
+			
+				echo $innerurl = file_get_html($localvar);
+				
+			
+		 
+	}
+	}
+	}}
+	else{
+		$html = file_get_html($FinalURL);
+	}
 ?>
