ff8c6442c0608ed836bda32626d3a96abec89742
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-24 12:03:57 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,9 +6,8 @@ $SiteURL = 'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 
 for($i = 0; $i > 3; $i++)
 	{
-		$Page = $SiteURL . $i;
-	$html = file_get_html($Page);
-	echo $html;
+		echo $Page = $SiteURL . $i;
+	
 	}
 
 
