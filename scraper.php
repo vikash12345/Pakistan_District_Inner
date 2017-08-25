e4ece2b891b4e78bd3e6db1b7e237bf5e252328e
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-25 17:24:09 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,7 @@ require 'scraperwiki/simple_html_dom.php';
 	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 	
 	//	Page pagination
-	for($PageLoop = 1; $PageLoop < 2; $PageLoop++){
+	for($PageLoop = 0; $PageLoop < 2; $PageLoop++){
 
 		$FinalURL	=	$SiteURL . $PageLoop;
 		$Html		=	file_get_html($FinalURL);
