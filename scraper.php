601b13a255ab4ebc6b65cf8148204f94db29e605
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-25 17:11:20 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -25,36 +25,8 @@ require 'scraperwiki/simple_html_dom.php';
 					$CaseLink	=	str_replace("amp;", "", $CaseLinkR);
 					
 					//	Visit link inside 'View' button
-					$DetailPg	=	file_get_html($CaseLink);
+				echo 	$DetailPg	=	file_get_html($CaseLink);
 
-					if ($DetailPg) {
-						//	Assign fields to varilables
-						$InstDte	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0)->plaintext;
-						$InstDte1st	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1)->plaintext;
-						$Status		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1)->plaintext;
-						$CaseFlDte	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 2)->plaintext;
-						$RestrCode	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 3)->plaintext;
-						$USCode		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 3)->plaintext;
-						$AdvPSide1	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 4)->plaintext;
-						$AdvPSide2	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 4)->plaintext;
-						
-						echo	"Institution Date = " . $InstDte . '<br/>';
-						echo 	"Institution Date 1st Court = " . $InstDte1st . '<br/>';
-						echo 	"Status = " . $Status . '<br/>';
-						echo 	"Current Case Flow Date = " . $CaseFlDte . '<br/>';
-						echo 	"Restore Code = " . $RestrCode . '<br/>';
-						echo 	"U/S Code = " . $USCode . '<br/>';
-						echo 	"Advocate Party Side 1 = " . $AdvPSide1 . '<br/>';
-						echo 	"Advocate Party Side 2 = " . $AdvPSide2 . '<br/>';
-						
-						//	Show a line betten records
-						echo	'<hr>';
-					}
-				}
-			}
-		}
-	}
-
-
+				}}}}
 
 ?>
